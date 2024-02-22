21988574c7fc7559b43bf1c59e87da16d1f5061f
ahmadekhalili <ahmadkhalili2020@gmail.com>
2024-02-22 09:38:39 +0330

    Updated environment variables of .env

--- mongo/init.js
+++ mongo/init.js
@@ -1,4 +1,4 @@
-db = db.getSiblingDB("${MONGO_NAME}");
+db = db.getSiblingDB("${MONGO_DBNAME}");
 db.init.insert({"welcome":"hello"})
-db.createUser({user: "${MONGO_USER}", pwd:  "${MONGO_PASSWORD}", roles: [{ role:"readWrite", db: "${MONGO_NAME}"}, { role:"read", db: "reporting"}]});
-db.createUser({user: "${MONGO_ADMINUSER}", pwd:  "${MONGO_PASSWORD}", roles: [{ role:"root", db: "admin"}]});
+db.createUser({user: "${MONGO_USERNAME}", pwd:  "${MONGO_PASSWORD}", roles: [{ role:"readWrite", db: "${MONGO_DBNAME}"}, { role:"read", db: "reporting"}]});
+db.createUser({user: "${MONGO_ADMINUSER}", pwd:  "${MONGO_USERPASS}", roles: [{ role:"root", db: "admin"}]});
